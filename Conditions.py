6288c87425b683cbc25b78b1830218aa46fad2bd
CyrilJoliet <124933559+CyrilJoliet@users.noreply.github.com>
2024-11-03 15:32:31 +0100

    Add files via upload

--- Conditions.py
+++ Conditions.py
@@ -16,6 +16,8 @@ import matplotlib.colors as mcolors
 from datetime import timedelta
 from sklearn.linear_model import LinearRegression
 import warnings
+import os
+from PIL import Image
 warnings.filterwarnings("ignore")
 
 
@@ -28,27 +30,32 @@ start = df['DATETIME'][0]
 df['TIME'] = [(start + timedelta(seconds=i)) for i in range(len(df))]
 
 
-
-
 # Fonction pour traiter les données
-def process_data(start_time, end_time):
+def process_data(start_time, end_time, fibre='B'):
+    # Vérification de la validité de l'argument fibre
+    if fibre not in ['B', 'D']:
+        raise ValueError("Le paramètre 'fibre' doit être 'B' ou 'D'")
+    
+    # Filtrer les données sur la période souhaitée
     collage_time = df[(df['TIME'] >= start_time) & (df['TIME'] <= end_time)]
     
+    # Colonnes à garder, adaptant MxB/FxB ou MxD/FxD selon fibre
     colonnes_a_garder = ['DATE', 'TIME', 'SPEED', 'WIDTH', 'LENGTH'] + \
-                        [f'M{i}B' for i in range(1, 46)] + [f'F{i}B' for i in range(1, 46)]
+                        [f'M{i}{fibre}' for i in range(1, 46)] + [f'F{i}{fibre}' for i in range(1, 46)]
 
     collage_fibre = collage_time[colonnes_a_garder]
 
-    colonnes_temp = [f'M{i}B' for i in range(1, 46)] + [f'F{i}B' for i in range(1, 46)]
+    # Colonnes pour les variations de température (BxB ou DxD selon fibre)
+    colonnes_temp = [f'M{i}{fibre}' for i in range(1, 46)] + [f'F{i}{fibre}' for i in range(1, 46)]
 
     # Calculer les variations de température
     for col in colonnes_temp:
-        collage_fibre.loc[:, f'variation_{col}'] = collage_fibre[col].diff()
+        collage_fibre[f'variation_{col}'] = collage_fibre[col].diff()
 
     n_mesures = 61  # Nombre de mesures (secondes)
     n_capteurs = 90
 
-    # Coordonnées X Y
+    # Coordonnées X et Y
     Y = np.arange(n_mesures) * 100  
     X = [i * 50 for i in range(45)] + [(i * 50 + 228.5 + 50 * 44) for i in range(45)] 
     final_df = pd.DataFrame(columns=['X', 'Y', 'Variation_t'])
@@ -58,35 +65,34 @@ def process_data(start_time, end_time):
             temp_df = pd.DataFrame({
                 'X': [X[i]] * n_mesures,             
                 'Y': Y,                              
-                'Variation_t': collage_fibre[f'variation_M{i+1}B'],  # Variation pour M
-                'MxB': [f'M{i+1}B'] * n_mesures      # Nom du capteur M
+                'Variation_t': collage_fibre[f'variation_M{i+1}{fibre}'],  # Variation pour M
+                'MxB': [f'M{i+1}{fibre}'] * n_mesures  # Nom du capteur M
             })
         else:
             temp_df = pd.DataFrame({
                 'X': [X[i]] * n_mesures,             
                 'Y': Y,                              
-                'Variation_t': collage_fibre[f'variation_F{i-44}B'],  # Variation pour F
-                'MxB': [f'F{i-44}B'] * n_mesures     # Nom du capteur F
+                'Variation_t': collage_fibre[f'variation_F{i-44}{fibre}'],  # Variation pour F
+                'MxB': [f'F{i-44}{fibre}'] * n_mesures  # Nom du capteur F
             })
 
         final_df = pd.concat([final_df, temp_df], ignore_index=True)
-        
-    # Filtrer les capteurs
+
+    # Filtrer les capteurs en fonction de fibre
     final_df = final_df[
-        ~final_df['MxB'].isin([f'M{i}B' for i in list(range(1, 9)) + list(range(35, 46))]) &  # Retirer M1B-M8B, M38B-M45B
-        ~final_df['MxB'].isin([f'F{i}B' for i in list(range(1, 12)) + list(range(39, 46))])    # Retirer F1B-F8B, F38B-F45B
+        ~final_df['MxB'].isin([f'M{i}{fibre}' for i in list(range(1, 9)) + list(range(36, 46))]) &  # Retirer M1x-M8x, M38x-M45x
+        ~final_df['MxB'].isin([f'F{i}{fibre}' for i in list(range(1, 11)) + list(range(38, 46))])    # Retirer F1x-F8x, F38x-F45x
     ]
 
-    # Décaler les positions X des capteurs FxB
-    final_df.loc[final_df['MxB'].str.startswith('F'), 'X'] += -1100
-
+    # Décaler les positions X des capteurs FxD (ou FxB)
+    final_df.loc[final_df['MxB'].str.startswith('F'), 'X'] += -1000
 
     return final_df
 
-# Générer les plages de temps avec un décalage de 1 seconde
+# Générer les plages de temps de 23:01:00 à 23:03:00 avec un décalage de 1 seconde
 time_ranges = []
-start_time = pd.to_datetime("06-25-24 22:14:30")
-for i in range(3600):  
+start_time = pd.to_datetime("06-25-24 23:01:25")
+for i in range(15):  # 121 secondes de 23:01:00 à 23:03:00
     start_time_str = (start_time + timedelta(seconds=i))
     end_time_str = (start_time + timedelta(seconds=i + 60))
     time_ranges.append((start_time_str, end_time_str))
@@ -95,24 +101,47 @@ for i in range(3600):
 
 #------------------------------ Avec simulateur tableau de bord
 
+  
+# Fonction pour calculer l'angle entre deux pentes
+image_folder = 'temp_images'
+if not os.path.exists(image_folder):
+    os.makedirs(image_folder)
+
 orange_condition_counter = 0
 
-alarme_rouge = []
-alarme_orange = []
-
-from tqdm import tqdm
 # Boucle sur les plages horaires
-for start_time, end_time in tqdm(time_ranges, desc="Processing time ranges"): 
-    final_df = process_data(start_time, end_time)
+for frame, (start_time, end_time)    in enumerate(time_ranges): 
+    final_df = process_data(start_time, end_time,fibre="B")
     
     seuil = 1
     filtered_df = final_df[final_df['Variation_t'] > seuil]
     fil_df = final_df[final_df['Variation_t'] < 0]
 
     # Vérifier s'il y a des points filtrés avant de continuer
-    if not filtered_df.empty:
+    if filtered_df.empty:
+        norm = mcolors.TwoSlopeNorm(vmin=-1., vmax=2, vcenter=0)
+        cmap = plt.get_cmap('coolwarm')  
+        
+        fig, ax = plt.subplots(figsize=(10, 6)) #plt.figure(figsize=(10, 6))
+        scatter = plt.scatter(final_df['X'], final_df['Y'], c=final_df['Variation_t'], s=25, cmap=cmap, norm=norm)
+        plt.xlim([300, 3600])  
+        plt.ylim([-100, 6300])
+        
+        #Déterminer la couleur de fond en fonction des conditions
+        background_color = 'green'  # Par défaut
+        fig.patch.set_facecolor(background_color)  # Changer la couleur de fond ici
+        norm = mcolors.TwoSlopeNorm(vmin=-1., vmax=2, vcenter=0)
+        cmap = plt.get_cmap('coolwarm')
+
+        cbar = plt.colorbar(scatter)
+        cbar.set_label('Δ')
+        plt.title(f'{end_time}')
+        
+        plt.savefig(f'{image_folder}/image_{frame}.png')
+        plt.show()
         
         
+    else: 
         # 2. Trouver le point de la base du "V" (celui avec le plus petit Y)
         min_Y_point = filtered_df.loc[filtered_df['Y'].idxmin()]
 
@@ -142,14 +171,20 @@ for start_time, end_time in tqdm(time_ranges, desc="Processing time ranges"):
         y_right_pred = model_right.predict(X_right)
         slope_right = model_right.coef_[0]
         r2r = model_right.score(X_right, y_right)
-
-
+        
+        
+        
+        
+         
         L = final_df[(final_df['X'] >= int(X_left.min())) & (final_df['X'] <= int(X_left.max()))]['X'].unique()
         L = list(L)
     
         R = final_df[(final_df['X'] >= int(X_right.min())) & (final_df['X'] <= int(X_right.max()))]['X'].unique()
         R = list(R)
     
+        
+        
+        
         variations_before_peak_L = []
         variations_after_peak_L = []
         variations_before_peak_R = []
@@ -211,45 +246,53 @@ for start_time, end_time in tqdm(time_ranges, desc="Processing time ranges"):
         # Vérification des pentes opposées et de l'angle
         if  -10 <= slope_left <= -2 and 2 <= slope_right<= 10 and len(close_high)>20 and r2l>0.5 and r2r>0.5 and (mbL>maL or mbR>maR):
         
-            orange_condition_counter += 1
-            if orange_condition_counter > 5:
+                orange_condition_counter += 1
+                if orange_condition_counter > 5:
                     background_color = 'red'
                     print(f"Alarme rouge à {end_time.time()}")
-                    
-                    alarme_rouge.append(end_time)
-                    
-                    fig, ax = plt.subplots(figsize=(10, 6))
-                    fig.patch.set_facecolor(background_color)  # Changer la couleur de fond ici
-
-                    norm = mcolors.TwoSlopeNorm(vmin=-1., vmax=2, vcenter=0)
-                    cmap = plt.get_cmap('coolwarm')
-
-                    scatter = ax.scatter(final_df['X'], final_df['Y'], c=final_df['Variation_t'], s=25, cmap=cmap, norm=norm)
-
-                    # Tracer la droite de régression pour la partie gauche
-                    ax.plot(X_left, y_left_pred, color='black', label=f'Régression gauche\nPente: {slope_left:.2f}')
-                    # Tracer la droite de régression pour la partie droite
-                    ax.plot(X_right, y_right_pred, color='black', label=f'Régression droite\nPente: {slope_right:.2f}')
-                    
-                    plt.xlim([300, 3550])  
-                    plt.ylim([-100, 6200])
-
-                    cbar = plt.colorbar(scatter)
-                    cbar.set_label('Variation de Température (°C)')
-                    plt.title(f'Variation de Température de {start_time.time()} à {end_time.time()}')
-
-                    plt.legend()
-                    plt.show()
-            else:
+                else:
                     background_color = 'orange'
-                    alarme_orange.append(end_time)
                     print(f"Alarme orange à {end_time.time()}")
-            
+         # Réinitialiser le compteur si la condition n'est plus remplie
         else:
             orange_condition_counter = 0
             
             
-         
-      
+            
+            # 6. Création du graphique avec changement de couleur de fond
+        fig, ax = plt.subplots(figsize=(10, 6))
+        fig.patch.set_facecolor(background_color)  # Changer la couleur de fond ici
 
+        norm = mcolors.TwoSlopeNorm(vmin=-1., vmax=2, vcenter=0)
+        cmap = plt.get_cmap('coolwarm')
+
+        scatter = ax.scatter(final_df['X'], final_df['Y'], c=final_df['Variation_t'], s=25, cmap=cmap, norm=norm)
+
+        # Tracer la droite de régression pour la partie gauche
+        ax.plot(X_left, y_left_pred, color='black', label=f'Régression gauche\nPente: {slope_left:.2f}')
+        # Tracer la droite de régression pour la partie droite
+        ax.plot(X_right, y_right_pred, color='black', label=f'Régression droite\nPente: {slope_right:.2f}')
         
+        plt.xlim([300, 3600])  
+        plt.ylim([-100, 6300])
+
+        cbar = plt.colorbar(scatter)
+        cbar.set_label('Δ')
+        plt.title(f'{end_time}')
+
+        plt.savefig(f'{image_folder}/image_{frame}.png')
+        plt.show()
+        
+images = []
+for frame in range(len(time_ranges)):
+    img_path = f'{image_folder}/image_{frame}.png'
+    images.append(Image.open(img_path))
+    
+# Sauvegarder l'animation
+gif_path = 'temperature_variation2.gif'
+images[0].save(gif_path, save_all=True, append_images=images[1:], duration=1000, loop=1)
+
+# Supprimer les images temporaires si nécessaire
+for img in os.listdir(image_folder):
+    os.remove(os.path.join(image_folder, img))
+os.rmdir(image_folder)
